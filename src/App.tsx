831d08e30a22187720849f1665414965515772bc
PotterFH <ivan_2333@outlook.com>
2024-04-19 20:11:53 -0600

    primer commit

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import { useState, useReducer } from 'react'
+import Form from './components/Form';
+import { activityReducer, initialState } from './reducers/activity-reducer';
+
+function App() {
+
+  // const [count, setCount] = useState(0)
+  const [state, dispatch] = useReducer(activityReducer, initialState)
+
+  return (
+    <>
+      <header className='bg-lime-400 py-3'>
+        <div className='max-w-4xl mx-auto flex justify-between'>
+          <h1 className='text-center text-lg font-bold text-white uppercase'>
+            Contador de calorias
+          </h1>
+        </div>
+      </header>
+
+      <section className='bg-lime-500 py-20 px-5'>
+        <div className='max-w-4xl mx-auto'>
+          <Form
+            dispatch={dispatch}
+          />
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default App
