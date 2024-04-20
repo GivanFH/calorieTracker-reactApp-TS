ced179eb2d380345808597ee9afe68b3a8cc0e7d
PotterFH <ivan_2333@outlook.com>
2024-04-19 22:44:58 -0600

    Editar actividad

--- src/components/ActivityList.tsx
+++ src/components/ActivityList.tsx
@@ -1,13 +1,15 @@
 import { useMemo } from "react"
 import { Activity } from "../types"
-import { Category } from '../types/index';
 import { categories } from "../data/categories";
+import { PencilSquareIcon } from '@heroicons/react/24/outline'; 
+import { ActivityActions } from "../reducers/activity-reducer";
 
 type ActivityListProps = {
     activities: Activity[]
+    dispatch: React.Dispatch<ActivityActions>
 }
 
-export default function ActivityList({ activities }: ActivityListProps) {
+export default function ActivityList({ activities, dispatch }: ActivityListProps) {
 
     const categoryName = useMemo(() => (category: Activity['category']) => categories.map( cat => cat.id === category ? cat.name : '') , [activities])
     return (
@@ -24,8 +26,13 @@ export default function ActivityList({ activities }: ActivityListProps) {
                             <span>Calorias</span>
                         </p>
                     </div>
-                    <div>
+                    <div className="flex gap-5 items-center">
+                        <button onClick={() => dispatch({type: 'set-activeId', payload: {id: activity.id}})}>
+                            <PencilSquareIcon
+                                className="h-8 w-8 text-gray-800"
 
+                            />
+                        </button>
                     </div>
                 </div>
             ))}
--- src/reducers/activity-reducer.ts
+++ src/reducers/activity-reducer.ts
@@ -1,14 +1,17 @@
 import { Activity } from "../types"
 
 export type ActivityActions =
-    { type: 'save-activity', payload: { newActivity: Activity } }
+    { type: 'save-activity', payload: { newActivity: Activity } } |
+    { type: 'set-activeId', payload: { id: Activity['id'] } }
 
-type ActivityState = {
-    activities: Activity[]
+export type ActivityState = {
+    activities: Activity[],
+    activeId: Activity['id']
 }
 
 export const initialState: ActivityState = {
-    activities: []
+    activities: [],
+    activeId: ''
 }
 
 export const activityReducer = (
@@ -18,9 +21,26 @@ export const activityReducer = (
     if (action.type === 'save-activity') {
 
         //Este codigo maneja la logica para actualizar el state
+        let updateActivities: Activity[] = []
+
+        if (state.activeId) {
+            updateActivities = state.activities.map(activity => activity.id === state.activeId ? action.payload.newActivity : activity)
+        } else {
+            updateActivities = [...state.activities, action.payload.newActivity]
+        }
+
         return {
             ...state,
-            activities: [...state.activities, action.payload.newActivity]
+            activities: updateActivities,
+            activeId : ''
+        }
+    }
+
+    if (action.type === 'set-activeId') {
+
+        return {
+            ...state,
+            activeId: action.payload.id
         }
     }
 
