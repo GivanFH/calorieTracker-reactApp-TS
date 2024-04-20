6fd94025ba87164f8f046376084af769cb305522
PotterFH <ivan_2333@outlook.com>
2024-04-19 21:41:13 -0600

    Listado de actividadesa

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState, useReducer } from 'react'
 import Form from './components/Form';
 import { activityReducer, initialState } from './reducers/activity-reducer';
+import ActivityList from './components/ActivityList';
 
 function App() {
 
@@ -23,6 +24,12 @@ function App() {
             dispatch={dispatch}
           />
         </div>
+      </section>
+
+      <section className='p-10 mx-auto bg-slate-100'>
+        <ActivityList
+          activities={state.activities}
+        />
       </section>
     </>
   )
