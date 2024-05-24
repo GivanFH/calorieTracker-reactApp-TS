fc3381e3d409936720adcc836acece742c77546c
PotterFH <ivan_2333@outlook.com>
2024-05-24 09:59:34 -0600

    Migrado a useReducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,54 @@
-import { useState, useReducer, useEffect, useMemo } from 'react'
-import Form from './components/Form';
-import { activityReducer, initialState } from './reducers/activity-reducer';
-import ActivityList from './components/ActivityList';
-import CalorieTracker from './components/CalorieTracker';
+import { useEffect, useMemo } from "react";
+import Form from "./components/Form";
+import ActivityList from "./components/ActivityList";
+import CalorieTracker from "./components/CalorieTracker";
+import { useActivity } from "./hooks/useActivity";
 
 function App() {
-
-  // const [count, setCount] = useState(0)
-  const [state, dispatch] = useReducer(activityReducer, initialState)
+  const { state, dispatch } = useActivity();
 
   useEffect(() => {
-    localStorage.setItem('activities', JSON.stringify(state.activities))
-  }, [state.activities])
+    localStorage.setItem("activities", JSON.stringify(state.activities));
+  }, [state.activities]);
 
-  const canRestartApp = () => useMemo(() => state.activities.length, [state.activities])
+  const canRestartApp = () =>
+    useMemo(() => state.activities.length, [state.activities]);
 
   return (
     <>
-      <header className='bg-lime-400 py-3'>
-        <div className='max-w-4xl mx-auto flex justify-between'>
-          <h1 className='text-center text-lg font-bold text-white uppercase'>
+      <header className="bg-lime-400 py-3">
+        <div className="max-w-4xl mx-auto flex justify-between">
+          <h1 className="text-center text-lg font-bold text-white uppercase">
             Contador de calorias
           </h1>
 
-          <button className='bg-gray-800 hove:bg-gray-900 p-2 font-bold uppercase text-white cursor-pointer rounded-lg text-sm disabled:opacity-10'
-           disabled={!canRestartApp()}
-           onClick={() => dispatch({type: 'restart-app'})}
-           >
+          <button
+            className="bg-gray-800 hove:bg-gray-900 p-2 font-bold uppercase text-white cursor-pointer rounded-lg text-sm disabled:opacity-10"
+            disabled={!canRestartApp()}
+            onClick={() => dispatch({ type: "restart-app" })}
+          >
             Reiniciar App
           </button>
         </div>
       </header>
 
-      <section className='bg-lime-500 py-20 px-5'>
-        <div className='max-w-4xl mx-auto'>
-          <Form
-            dispatch={dispatch}
-            state={state}
-          />
+      <section className="bg-lime-500 py-20 px-5">
+        <div className="max-w-4xl mx-auto">
+          <Form/>
         </div>
       </section>
 
-      <section className='bg-gray-600 py-10'>
-        <div className='max-w-4xl mx-auto'>
-          <CalorieTracker
-            activities={state.activities}
-          />
+      <section className="bg-gray-600 py-10">
+        <div className="max-w-4xl mx-auto">
+          <CalorieTracker />
         </div>
       </section>
 
-      <section className='p-10 mx-auto bg-slate-100'>
-        <ActivityList
-          activities={state.activities}
-          dispatch={dispatch}
-        />
+      <section className="p-10 mx-auto bg-slate-100">
+        <ActivityList />
       </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/ActivityList.tsx
+++ src/components/ActivityList.tsx
@@ -1,18 +1,10 @@
-import { useMemo } from "react"
-import { Activity } from "../types"
-import { categories } from "../data/categories";
 import { PencilSquareIcon, XCircleIcon } from '@heroicons/react/24/outline';
-import { ActivityActions } from "../reducers/activity-reducer";
+import { useActivity } from "../hooks/useActivity";
 
-type ActivityListProps = {
-    activities: Activity[]
-    dispatch: React.Dispatch<ActivityActions>
-}
 
-export default function ActivityList({ activities, dispatch }: ActivityListProps) {
+export default function ActivityList() {
 
-    const categoryName = useMemo(() => (category: Activity['category']) => categories.map(cat => cat.id === category ? cat.name : ''), [activities])
-    const isEmptyActivities = useMemo(() => activities.length === 0, [activities])
+    const { state, dispatch, isEmptyActivities, categoryName } = useActivity()
 
     return (
         <>
@@ -21,7 +13,7 @@ export default function ActivityList({ activities, dispatch }: ActivityListProps
 
             {isEmptyActivities ? <p className="text-center my-5">No hay actividades aun... </p> :
 
-                activities.map(activity => (
+                state.activities.map(activity => (
                     <div key={activity.id} className="px-5 py-10 bg-white mt-5 flex justify-between">
                         <div className="space-y-2 relative">
                             <p className={`absolute -top-8 -left-8 px-10 py-2 text-white uppercase font-bold ${activity.category === 1 ? 'bg-lime-500' : 'bg-orange-500'}`}>{categoryName(activity.category)}</p>
@@ -47,8 +39,6 @@ export default function ActivityList({ activities, dispatch }: ActivityListProps
                         </div>
                     </div>
                 ))}
-
-
         </>
     )
 }
